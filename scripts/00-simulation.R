d65a318a425e3be62dc5b78ae989508bb611c821
parkyunk <clara.park@mail.utoronto.ca>
2022-03-27 15:15:40 -0400

    Update scripts

--- scripts/00-simulation.R
+++ scripts/00-simulation.R
@@ -1,11 +1,15 @@
 #### Preamble ####
-# Purpose: Simulate
+# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
 # Author: Pascal Lee Slew, Yunkyung Park
-# Email: pascal.leeslew@mail.utoronto.ca, clara.park@mail.utoronto.ca
-# Date: 3 April 2022
-# Prerequisites: - 
+# Data: 3 April 2022
+# Contact: pascal.leeslew@mail.utoronto.ca, clara.park@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the ACS data and saved it to inputs/data
+# - Don't forget to gitignore it!
 
 #### Workspace set-up ####
 library(janitor)
 library(lubridate)
 library(tidyverse)
+
--- scripts/02-clean_and_prepare_data.R
+++ scripts/02-clean_and_prepare_data.R
@@ -1,28 +1,44 @@
 #### Preamble ####
+# Purpose: Clean and prepare the 1988 Ghana census data downloaded from the DHS Program
+# Author: Pascal Lee Slew, Yunkyung Park
+# Data: 3 April 2022
+# Contact: pascal.leeslew@mail.utoronto.ca, clara.park@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the 1988 Ghana census data by following the process 
+# in 01-gather_data.R and saved it to outputs/data
+# - Don't forget to gitignore it!
 
-library(janitor)
+#### Workspace set-up ####
 library(tidyverse)
 
-raw_data <- read_csv("raw_data.csv")
+raw_data <- read_csv("outputs/data/raw_data.csv")
 
 col_len <- ncol(raw_data)
   
 clean <- as_tibble(raw_data)
-clean$charact <- (rowSums(is.na(clean)) == col_len-1)
+clean$character <- (rowSums(is.na(clean)) == col_len-1)
 
 index <- 1:nrow(clean)
 j<-0
 while (j < nrow(clean)-1){
   j<-j+1
-  if (clean[j,]$charact == TRUE){
+  if (clean[j,]$character == TRUE){
     i<-j
   }
   
   index[j]<-clean[i,]$background
 }
 
-index[nrow(clean)]<-""
-clean$charact<-index
+index[nrow(clean)]<-"Total"
+clean$character<-index
 clean <-
   clean %>%
   na.omit()
+
+rm(col_len,
+   index,
+   i,
+   j)
+
+write_csv(clean, "outputs/data/cleaned_data.csv")
